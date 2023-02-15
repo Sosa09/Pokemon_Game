400e2998c34da835e76a245f59a8350e2c50af30
Sosa09 <soufiane.arrazouki@hotmail.com>
2023-02-15 21:11:38 +0100

    cmd

---
+++ Classes/start.py
@@ -0,0 +1,14 @@
+#Class for the Start menu and the choice of the 2 pokemons
+import pokemon
+class start:
+    def __init__(self, pokemons):
+        self.pokemons = pokemons
+        self.load_pokemons(self.pokemons)
+    
+    def load_pokemons(self):
+        
+        pass
+    
+    def show_start_pokemon(self):
+        pass
+    
