ffe67f59bf398797712773c4934447aea98b04fa
Sosa09 <soufiane.arrazouki@hotmail.com>
2023-02-15 21:11:02 +0100

    changes made

---
+++ Classes/map.py
@@ -0,0 +1,9 @@
+import pokemon
+class map:
+    def __init__(self):
+        pass
+    
+    def load_pokemons_on_map(self, pokemon):
+        #fill the map with pokemons
+        for poke in pokemon:
+            pass
