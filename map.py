ad1d790ca1eeab825a4372c541d8a42d7b274a51
K4LM4 <croonenburg.co@gmail.com>
2023-02-18 19:45:00 +0100

    black map

--- map.py
+++ map.py
@@ -1,3 +1,26 @@
-# breed  =  800
+import pygame, sys
+from settings import *
 
-# hoogte = 600 
+class Game:
+
+    def __init__(self):
+
+        #de Basics
+        pygame.init()
+        self.screen = pygame.display.set_mode((WIDTH,HEIGTH))
+        pygame.display.set_caption('POKEMON SYNTRA')
+        self.clock = pygame.time.Clock()
+
+    def run(self):
+        while True:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    sys.exit()
+            self.screen.fill('black')
+            pygame.display.update()
+            self.clock.tick(FPS)
+
+if __name__=='__main__':
+    game = Game()
+    game.run()
