76513c74fdf6217b140f58bb9e0e0678d81eeaee
K4LM4 <croonenburg.co@gmail.com>
2023-02-25 11:20:39 +0100

    collisions

--- level.py
+++ level.py
@@ -11,13 +11,13 @@ class Level:
         #get display
         self.display_surface = pygame.display.get_surface()
         #sprite group setup
-        self.visible_sprites = pygame.sprite.Group()
+        self.visible_sprites = YsortCameraGroup()
         self.obstacle_sprites = pygame.sprite.Group()
         self.create_map()
 
     def run(self):
         #update
-        self.visible_sprites.draw(self.display_surface)
+        self.visible_sprites.custom_draw(self.player)
         self.visible_sprites.update()
 
 
@@ -39,9 +39,23 @@ class Level:
                 if col == '4':
                     Pokemon4((x,y),[self.visible_sprites])
                 if col == 'z':
-                    self.player = Player((x,y),[self.visible_sprites])
+                    self.player = Player((x,y),[self.visible_sprites],self.obstacle_sprites)
                 
 
+class YsortCameraGroup(pygame.sprite.Group):
 
+    def __init__(self):
+        super().__init__()
+        self.display_surface = pygame.display.get_surface()
+        self.half_width = self.display_surface.get_size()[0]//2
+        self.half_heigth = self.display_surface.get_size()[1]//2
+        self.offset = pygame.math.Vector2()
 
+    def custom_draw(self,player):
 
+        self.offset.x = player.rect.centerx - self.half_width
+        self.offset.y = player.rect.centery - self.half_heigth
+
+        for sprite in self.sprites():
+            offset_pos = sprite.rect.topleft - self.offset
+            self.display_surface.blit(sprite.image,offset_pos)
