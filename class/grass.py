066467c3e3ed1a1f5e19aba8ab9d99ade3f79c57
Joris Ceuppens <jorisc50@gmail.com>
2023-05-29 11:48:25 +0200

    changed files

---
+++ class/grass.py
@@ -0,0 +1,9 @@
+
+import pygame
+
+class Grass(pygame.sprite.Sprite):
+
+    def __init__(self, pos, groups):
+        super().__init__(groups)
+        self.image = pygame.image.load('images/grass.png').convert_alpha()
+        self.rect = self.image.get_rect(topleft = pos)
---
+++ class/level.py
@@ -0,0 +1,64 @@
+import pygame
+from settings import  *
+from tile import Tile
+from grass import Grass
+from pokemon import *
+from player import Player
+
+class Level:
+
+    def  __init__(self):
+        #get display
+        self.display_surface = pygame.display.get_surface()
+        #sprite group setup
+        self.visible_sprites = YsortCameraGroup()
+        self.obstacle_sprites = pygame.sprite.Group()
+        self.pokemons = []
+        self.create_map()
+       
+
+    def run(self):
+        #update
+        self.visible_sprites.custom_draw(self.player)
+        self.visible_sprites.update()
+
+
+    def create_map(self):
+        for row_index,row in enumerate(WORLD_MAP):
+            for col_index, col in enumerate(row):
+                x = col_index * TILESIZE
+                y = row_index * TILESIZE
+                if col == 'x':
+                    Tile((x,y),[self.visible_sprites,self.obstacle_sprites])
+                if col == 'g':
+                    Grass((x,y),[self.visible_sprites])
+                if col == '1':
+                    self.pokemons.append(Pokemon1((x,y),[self.visible_sprites]))
+                if col == '2':
+                    self.pokemons.append(Pokemon2((x,y),[self.visible_sprites]))
+                if col == '3':
+                    self.pokemons.append(Pokemon3((x,y),[self.visible_sprites]))
+                if col == '4':
+                    self.pokemons.append(Pokemon4((x,y),[self.visible_sprites]))
+                if col == 'z':
+                    self.player = Player((x,y),[self.visible_sprites],self.obstacle_sprites)
+                    
+                
+
+class YsortCameraGroup(pygame.sprite.Group):
+
+    def __init__(self):
+        super().__init__()
+        self.display_surface = pygame.display.get_surface()
+        self.half_width = self.display_surface.get_size()[0]//2
+        self.half_heigth = self.display_surface.get_size()[1]//2
+        self.offset = pygame.math.Vector2()
+
+    def custom_draw(self,player):
+
+        self.offset.x = player.rect.centerx - self.half_width
+        self.offset.y = player.rect.centery - self.half_heigth
+
+        for sprite in self.sprites():
+            offset_pos = sprite.rect.topleft - self.offset
+            self.display_surface.blit(sprite.image,offset_pos)
